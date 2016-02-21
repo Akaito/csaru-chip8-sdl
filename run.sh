f925c68c70d0d1204e7f204fa00792b9cff9b648
Chris Barrett <chris@codesaru.com>
2016-02-20 20:37:54 -0800

    Change run.sh to Space Invaders program.

--- run.sh
+++ run.sh
@@ -18,22 +18,22 @@ progfile=Maze\ \[David\ Winter,\ 199x\].ch8
 
 progpath=~/chip8-roms/chip8.com/Chip-8\ Games/
 
-progfile=Blinky\ \[Hans\ Christian\ Egeberg,\ 1991\].ch8
+#progfile=Blinky\ \[Hans\ Christian\ Egeberg,\ 1991\].ch8
 #progfile=Blinky\ \[Hans\ Christian\ Egeberg\]\ \(alt\).ch8
 #progfile=Pong\ \(alt\).ch8
 #progfile=Space\ Flight.ch8
-#progfile=Space\ Invaders\ \[David\ Winter\]\ \(alt\).ch8
+progfile=Space\ Invaders\ \[David\ Winter\]\ \(alt\).ch8
 #progfile=Tetris\ \[Fran\ Dachille,\ 1991\].ch8
 #progfile=Tron.ch8
 #progfile=ZeroPong\ \[zeroZshadow,\ 2007\].ch8
 #progfile=Tank.ch8
 
 
-progpath=~/chip8/roms/
+#progpath=~/chip8/roms/
 
 #progfile=minimal.ch8
+#progfile=pong-story.com/UFO
 #progfile=pong-story.com/BLINKY
-progfile=pong-story.com/UFO
 
 chip8-sdl "${progpath}${progfile}"
 
